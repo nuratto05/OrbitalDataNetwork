235ffb71e22549a9002bbf203dd670b8b2b4076c
Nur Atto <nuratto05@gmail.com>
2026-03-28 18:52:28 -0500

    Create satellite class

---
+++ OrbitalSim/Models/Satellite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Satellite
+{
+	public int id { get; set; }
+	public string MissionType { get; set; }
+	public string Status { get; set; }
+	public double Lat { get; set; }
+	public double Lon { get; set; }
+	public double Altitude { get; set; }
+
+    Random rand = new Random();
+
+    public Satellite(string missionType, string status, double lat, double lon, double altitude)
+    {
+        id = rand.Next(1111, 2222);
+        MissionType = missionType;
+        Status = status;
+        Lat = lat;
+        Lon = lon;
+        Altitude = altitude;
+    }
+}
