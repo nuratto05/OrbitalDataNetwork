d586732f00209c9419253ed2efa0fc9a117e3088
Nur Atto <nuratto05@gmail.com>
2026-03-28 19:20:14 -0500

    Create Services File for Orbit and Telemetry Methods

---
+++ OrbitalSim/Services/OrbitService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OrbitalSim.Models;
+
+namespace OrbitalSim.Services
+{
+    public class Orbit
+    {
+        Random rand = new Random();
+
+        public void updateLocation(Satellite sat)
+        {
+            if (sat == null) return;
+
+            sat.Lat = rand.NextDouble();
+            sat.Lon = rand.NextDouble();
+
+        }
+    }
+}
---
+++ OrbitalSim/Services/TelemetryService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using OrbitalSim.Models;
+
+namespace OrbitalSim.Services
+{
+    public class TelemetryService
+    {
+        Random rand = new Random();
+
+        public Telemetry GetTelemetry(Satellite sat)
+        {
+            Telemetry tel = new Telemetry();
+
+            tel.Temperature = rand.Next(50, 70);
+            tel.Battery = rand.Next(0, 100);
+            tel.Velocity = rand.Next(100, 300);
+
+            return tel;
+        }
+    }
+}
