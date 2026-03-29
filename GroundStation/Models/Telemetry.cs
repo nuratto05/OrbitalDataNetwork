18e4f98ab86a5d988b3bbd99cbee669e1fa45112
Nur Atto <nuratto05@gmail.com>
2026-03-29 15:42:19 -0500

    ADD: Add Class for JSON deserialization

---
+++ GroundStation/Models/Telemetry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroundStation.Models
+{
+
+    public class Telemetry
+    {
+        public TelemetryData Data { get; set; }
+        public TelemetryLocation Location { get; set; }
+
+        public Telemetry() { }
+
+        public override string ToString()
+        {
+            return $"TelemetryData: {Data}, TelemetryLocation: {Location}";
+        }
+    }
+}
---
+++ GroundStation/Models/TelemetryData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GroundStation.Models
+{
+    public class TelemetryData
+    {
+        public DateTime TimeStamp { get; set; }
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public double Temperature { get; set; }
+        public int Battery { get; set; }
+        public double Velocity { get; set; }
+
+        public TelemetryData()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"[Timestamp: {TimeStamp}] IDN: {Id}, Status: {Status}, Temperature: {Temperature}, Battery: {Battery}, Velocity: {Velocity}";
+        }
+    }
+}
