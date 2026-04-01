52f305d10a5bc4a2aac3dda4e10595cefaff95d6
Nur Atto <atton@augsburg.edu>
2026-04-01 16:06:53 -0500

    test commit

--- OrbitalSim/Program.cs
+++ OrbitalSim/Program.cs
@@ -17,7 +17,7 @@ namespace OrbitalSim
 
             var tel = OrbitSimulation.OrbitSimulationEngine(telemetryQueue);
             var send = TcpNetwork.SendTelViaTCP(telemetryQueue);
-
+             
             await Task.WhenAll(tel, send);
         }
     }
